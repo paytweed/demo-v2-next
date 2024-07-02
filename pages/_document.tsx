89bf53ff32affdaff817f06fb895cb475d061406
Max G <codeswhite@protonmail.com>
2024-07-02 13:20:35 +0300

    Redeploy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,17 +4,17 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-      <link
-					rel='icon'
-					href='/tweed-logo-dark.ico'
-					sizes='any'
-				/>
-				<link
-					rel='apple-touch-icon'
-					href='/tweed-logo-dark.ico'
-					type='image/x-icon'
-					sizes='any'
-				/>
+        <link
+          rel='icon'
+          href='/tweed-logo-dark.ico'
+          sizes='any'
+        />
+        <link
+          rel='apple-touch-icon'
+          href='/tweed-logo-dark.ico'
+          type='image/x-icon'
+          sizes='any'
+        />
         <title>Tweed Demo</title>
         <link
           href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
